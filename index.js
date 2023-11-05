1c2c3ba2fc2e58e056731e374ef0dd573b22a7fa
Austin Vichich <avichich4@gmail.com>
2023-11-05 11:47:09 -0700

    lab refresh

--- index.js
+++ index.js
@@ -1,53 +1,28 @@
-// Write your solution in this file!
 const employee = {
     name : "Sam",
     streetAddress : "11 Broadway"
 };
-
-console.log(employee);
-
 function updateEmployeeWithKeyAndValue(obj, key, value) {
     return {
         ...employee,
         [key]:value,
     };
-}
-const obj = updateEmployeeWithKeyAndValue(
-    employee,
-    "Sam",
-    "11 Broadway"
-);
+};
 
-console.log(employee.name);
-console.log(employee.streetAddress);
 
-function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
-    obj[key] = value;
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+};
 
-    return obj;
-}
-console.log(employee.name = 'Sam');
-console.log(employee.streetAddress = '12 Broadway');
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+};
+delete employee.key;
 
-function deleteFromEmployeeByKey(obj, key) {
-    const newObj = { ...obj };
-    delete newObj[key];
-    return newObj;
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
   }
-  
-  const newObj = deleteFromEmployeeByKey(employee, 'streetAddress');
-  
-  console.log(newObj.name);
-  console.log(newObj.streetAddress);
-
-
-
-  function destructivelyDeleteFromEmployeeByKey(obj, key) {
-    delete obj[key];
-    return obj;
-  }
-  
-  const newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
-  
-  console.log(newEmployee.name);
-
